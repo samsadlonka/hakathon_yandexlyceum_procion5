787b42a75757cdebdde074898505d4f114e7f13c
artem <makarenkoiandeks@yandex.ru>
2021-06-21 20:04:10 +0400

    v1.2

--- function_Brezenhema.py
+++ function_Brezenhema.py
@@ -16,7 +16,24 @@ def func(x0, y0, x1, y1): #x0, yo - координаты начала, x1, y1 - 
         if error >= (deltax + 1):
             y = y + diry
             error = error - (deltax + 1)
+    x = x0
+    deltaerr = (deltax + 1)
+    error = 0
+    dirx = x1 - x0
+    if dirx > 0:
+        dirx = 1
+    if dirx < 0:
+        dirx = -1
+    for y in range(y0, y1 + 1):
+        if [x, y] not in arr:
+            arr.append([x, y])
+        error = error + deltaerr
+        if error >= (deltay + 1):
+            x = x + dirx
+            error = error - (deltay + 1)
     return arr
 
 if __name__ == '__main__':
-    print(func(1, 5, 11, 1))
+    p = func(1, 5, 11, 1)
+    p.sort()
+    print(p)
